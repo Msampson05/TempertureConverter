ab629707c982eb63245486b421ae60afdeea6192
Megan Sampson <msampson@nyc.yearup.org>
2022-10-24 22:04:34 -0400

    added content

--- scripts/mortgage.js
+++ scripts/mortgage.js
@@ -4,22 +4,21 @@
 window.onload = init;
 function init() {
     
-//Find HTML elements 
-const principalinputField = document.getElementById("principalinputField");
-const  interestRateInputField= document.getElementById("interestRateInputField");
-const loanLengthInputField = document.getElementById("oanLengthInputField");
-
-
  // Calculate Button 
  const Calculatetbtn = document.getElementById("Calculatetbtn");
  Calculatetbtn.onclick = onCalculatebtnClicked;
 }
-function onCalculatetbtnClicked(){
-    const fahrenheitInputField = document.getElementById("fahrenheitInputField");
-    let fahrenheitInput = Number(fahrenheitInputField.value);
-    let answer = ((5/9) * (fahrenheitInput - 32));
-    const celsiusInputField = document.getElementById("celsiusInputField");
-    celsiusInputField.value = answer
+function onCalculatebtnClicked(){
+    //Find HTML elements 
+const principalinputField = document.getElementById("principalinputField");
+const  interestRateInputField= document.getElementById("interestRateInputField");
+const loanLengthInputField = document.getElementById("LoanLengthInputField");
 
+    let principalInput = Number(principalInput.value);
+    let interestInput = Number(interestInput.value);
+    let loanLenthInput = Number(loanLenthInput.value);
+    let monthlyInput = (principalInput)*(interestInput/ 1 -(1+interestInput)-loanLenthInput);
+    const  MonthlyInput= document.getElementById("MonthlyInput");
+    MonthlyInput.value = monthlyInput;
 
 }
